2f1dc2c83994a37cc3a89904030363ae343b84f0
manggaperas <muhammadanggaprasetyo12@gmail.com>
2023-07-11 08:33:15 +0700

    add new search function to app

--- RecruitmentTracking/Controllers/HomeController.cs
+++ RecruitmentTracking/Controllers/HomeController.cs
@@ -9,110 +9,106 @@ namespace RecruitmentTracking.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ApplicationDbContext _context;
-    private readonly ILogger<HomeController> _logger;
-    private readonly IConfiguration _configuration;
-    private readonly UserManager<User> _userManager;
+	private readonly ApplicationDbContext _context;
+	private readonly ILogger<HomeController> _logger;
+	private readonly IConfiguration _configuration;
+	private readonly UserManager<User> _userManager;
 
-    public HomeController(ILogger<HomeController> logger, IConfiguration configuration, ApplicationDbContext context, UserManager<User> userManager)
-    {
-        _logger = logger;
-        _configuration = configuration;
-        _context = context;
-        _userManager = userManager;
-    }
+	public HomeController(ILogger<HomeController> logger, IConfiguration configuration, ApplicationDbContext context, UserManager<User> userManager)
+	{
+		_logger = logger;
+		_configuration = configuration;
+		_context = context;
+		_userManager = userManager;
+	}
 
-    [HttpGet]
-    public async Task<IActionResult> Index()
-    {
-        User user = (await _userManager.GetUserAsync(User))!;
+	[HttpGet]
+	public async Task<IActionResult> Index()
+	{
+		User user = (await _userManager.GetUserAsync(User))!;
 
-        if (user != null)
-        {
-            if (await _userManager.IsInRoleAsync(user, "Admin")) return Redirect("/Admin");
-            Candidate objCandidate = (await _context.Candidates.FirstOrDefaultAsync(c => c.UserId == user.Id))!;
-            if (objCandidate == null)
-            {
-                TempData["warning"] = "Please complete your data";
-                return Redirect("/Profile");
-            }
-        }
+		if (user != null)
+		{
+			if (await _userManager.IsInRoleAsync(user, "Admin")) return Redirect("/Admin");
+			Candidate objCandidate = (await _context.Candidates.FirstOrDefaultAsync(c => c.UserId == user.Id))!;
+			if (objCandidate == null)
+			{
+				TempData["warning"] = "Please complete your data";
+				return Redirect("/Profile");
+			}
+		}
 
-        List<JobViewModel> listJob = new();
-        foreach (Job job in _context.Jobs!.Where(j => j.IsJobAvailable).ToList())
-        {
-            JobViewModel data = new()
-            {
-                JobId = job.JobId,
-                JobTitle = job.JobTitle,
-                JobDescription = job.JobDescription,
-                JobRequirement = job.JobRequirement,
-                Location = job.Location,
-                JobPostedDate = job.JobPostedDate,
-                JobExpiredDate = job.JobExpiredDate,
-                CandidateCout = job.candidateCount,
-            };
+		List<JobViewModel> listJob = new();
+		foreach (Job job in _context.Jobs!.Where(j => j.IsJobAvailable).ToList())
+		{
+			JobViewModel data = new()
+			{
+				JobId = job.JobId,
+				JobTitle = job.JobTitle,
+				JobDescription = job.JobDescription,
+				JobRequirement = job.JobRequirement,
+				Location = job.Location,
+				JobPostedDate = job.JobPostedDate,
+				JobExpiredDate = job.JobExpiredDate,
+				CandidateCout = job.candidateCount,
+			};
 
-            listJob.Add(data);
-        }
-        return View(listJob);
-    }
+			listJob.Add(data);
+		}
+		return View(listJob);
+	}
 
-    [HttpPost("/Search")]
-    public async Task<IActionResult> Index(string searchString)
-    {
-        Console.WriteLine($"{searchString}");
+	[HttpPost("/Search")]
+	public async Task<IActionResult> SearchJob(string searchstring)
+	{
+		var jobs = from j in _context.Jobs select j;
 
-        if (_context.Jobs == null)
-        {
-            return Problem("not found");
-        }
+		List<JobViewModel> listJob = new();
 
-        var jobs = from m in _context.Jobs
-                   select m;
+		if (!String.IsNullOrEmpty(searchstring))
+		{
+			var filteredjobs = jobs.ToList().Where(j => j.JobTitle != null && j.JobTitle.Contains(searchstring, StringComparison.OrdinalIgnoreCase));
+			foreach (var job in filteredjobs)
+			{
+				JobViewModel data = new()
+				{
+					JobId = job.JobId,
+					JobTitle = job.JobTitle,
+					JobDescription = job.JobDescription,
+					JobRequirement = job.JobRequirement,
+					Location = job.Location,
+					CandidateCout = job.candidateCount,
+				};
+				
+				listJob.Add(data);
+			}
+		}
+		System.Console.WriteLine(listJob.Count);
+		return View(listJob);
+	}
 
-        List<JobViewModel> listJob = new();
-        foreach (Job job in jobs.Where(s => s.JobTitle != null && s.JobTitle.Contains(searchString, StringComparison.OrdinalIgnoreCase)).ToList())
-        {
-            JobViewModel data = new()
-            {
-                JobId = job.JobId,
-                JobTitle = job.JobTitle,
-                JobDescription = job.JobDescription,
-                JobRequirement = job.JobRequirement,
-                Location = job.Location,
-                JobPostedDate = job.JobPostedDate,
-                JobExpiredDate = job.JobExpiredDate,
-                CandidateCout = job.candidateCount,
-            };
-            listJob.Add(data);
-        }
+	[HttpGet("/DetailJob/{id}")]
+	public IActionResult DetailJob(int id)
+	{
+		Job objJob = _context.Jobs!.Find(id)!;
 
-        return View(listJob);
-    }
+		JobViewModel data = new()
+		{
+			JobId = objJob.JobId,
+			JobTitle = objJob.JobTitle,
+			JobDescription = objJob.JobDescription,
+			JobRequirement = objJob.JobRequirement,
+			Location = objJob.Location,
+			JobPostedDate = objJob.JobPostedDate,
+			JobExpiredDate = objJob.JobExpiredDate,
+		};
 
-    [HttpGet("/DetailJob/{id}")]
-    public IActionResult DetailJob(int id)
-    {
-        Job objJob = _context.Jobs!.Find(id)!;
+		return View(data);
+	}
 
-        JobViewModel data = new()
-        {
-            JobId = objJob.JobId,
-            JobTitle = objJob.JobTitle,
-            JobDescription = objJob.JobDescription,
-            JobRequirement = objJob.JobRequirement,
-            Location = objJob.Location,
-            JobPostedDate = objJob.JobPostedDate,
-            JobExpiredDate = objJob.JobExpiredDate,
-        };
-
-        return View(data);
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
